1b02e5b2dd0544e46da3bef0f2343c6589f62359
Brennda Landim <125628894+brenndalandim@users.noreply.github.com>
2024-06-30 19:53:38 -0300

    update api lik

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,6 +1,6 @@
 
 async function fetchProfileData(){
-    let url = "https://raw.githubusercontent.com/brenndalandim/Portfolio_Dio/main/profile.json?token=GHSAT0AAAAAACUHYSMT23WVVOXGEGGLX7CWZUB3UGQ"
+    let url = "https://raw.githubusercontent.com/brenndalandim/Portfolio_Dio/main/profile.json"
 
     let fetching =  await fetch(url)
     return await fetching.json()
